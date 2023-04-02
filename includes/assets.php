18fa49af2d38f3c6cbefe71b86b3c197425f4dc0
Ronan <ronan56.hello@gmail.com>
2023-04-02 15:49:56 +0200

    update: Apply CSS and JS only on plugin pages

--- includes/assets.php
+++ includes/assets.php
@@ -1,7 +1,12 @@
 <?php
 
-add_action( 'admin_init', 'osk_admin_enqueue_scripts' );
-function osk_admin_enqueue_scripts() {
+add_action( 'admin_enqueue_scripts', 'osk_admin_enqueue_scripts' );
+function osk_admin_enqueue_scripts( $hook ) {
+
+	if ( ! str_contains( $hook, 'site-kit' ) ) {
+		return;
+	}
+
 	wp_enqueue_style( 'osk-app', plugins_url( '/admin/css/app.css', __DIR__ ), array(), OPENMOSTSITEKIT_VERSION );
 	//wp_enqueue_script('osk-app', plugins_url( '/dist/js/app.js', __DIR__ ), array(), OPENMOSTSITEKIT_VERSION, true );
 	wp_enqueue_script( 'osk-echarts', plugins_url( '/admin/js/echarts.js', __DIR__ ), array(), OPENMOSTSITEKIT_VERSION, true );
