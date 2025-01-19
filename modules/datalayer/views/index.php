bd0e89c66184f5d7d5228e44fb3e97953a9d44b0
Ronan HELLO <ronan56.hello@gmail.com>
2025-01-19 14:25:08 +0100

    remove: DataLayer module

--- modules/datalayer/views/index.php
+++
@@ -1,8 +0,0 @@
-<div class="wrap">
-	<h1 class="wp-heading-inline"><?php echo esc_html(get_admin_page_title()); ?></h1>
-	<form method="post" action="options.php" novalidate="novalidate">
-		<?php settings_fields('omsk-data-layer-settings-group'); ?>
-		<?php do_settings_sections('omsk-data-layer-settings'); ?>
-		<?php submit_button(); ?>
-	</form>
-</div>
